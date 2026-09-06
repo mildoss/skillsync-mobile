9b490f06188061d3a012ff330ae87175a992e9f7
mildoss <vlad.maslo.05@gmail.com>
2026-09-06 12:45:35 +0300

    feat(theme): configure color palette identical to web version

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,56 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background), <alpha-value>)",
+        foreground: "rgba(var(--foreground), <alpha-value>)",
+        card: {
+          DEFAULT: "rgba(var(--card), <alpha-value>)",
+          foreground: "rgba(var(--card-foreground), <alpha-value>)",
+        },
+        popover: {
+          DEFAULT: "rgba(var(--popover), <alpha-value>)",
+          foreground: "rgba(var(--popover-foreground), <alpha-value>)",
+        },
+        primary: {
+          DEFAULT: "rgba(var(--primary), <alpha-value>)",
+          foreground: "rgba(var(--primary-foreground), <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "rgba(var(--secondary), <alpha-value>)",
+          foreground: "rgba(var(--secondary-foreground), <alpha-value>)",
+        },
+        muted: {
+          DEFAULT: "rgba(var(--muted), <alpha-value>)",
+          foreground: "rgba(var(--muted-foreground), <alpha-value>)",
+        },
+        accent: {
+          DEFAULT: "rgba(var(--accent), <alpha-value>)",
+          foreground: "rgba(var(--accent-foreground), <alpha-value>)",
+        },
+        destructive: {
+          DEFAULT: "rgba(var(--destructive), <alpha-value>)",
+          foreground: "rgba(var(--destructive-foreground), <alpha-value>)",
+        },
+        border: "rgba(var(--border), <alpha-value>)",
+        input: "rgba(var(--input), <alpha-value>)",
+        ring: "rgba(var(--ring), <alpha-value>)",
+        success: {
+          DEFAULT: "rgba(var(--success), <alpha-value>)",
+          foreground: "rgba(var(--success-foreground), <alpha-value>)",
+        },
+        warning: {
+          DEFAULT: "rgba(var(--warning), <alpha-value>)",
+          foreground: "rgba(var(--warning-foreground), <alpha-value>)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [],
 };
